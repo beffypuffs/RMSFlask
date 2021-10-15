3ac4c0f15bfbf2819ffd1b691017ecdfa33fc421
beffypuffs <56742527+beffypuffs@users.noreply.github.com>
2021-10-15 11:32:28 -0700

    Uploading so I can work from remote desktop. Incomplete
    Will update shortly

--- RMSapp.py
+++ RMSapp.py
@@ -1,6 +1,5 @@
 from flask import Flask, redirect, url_for, render_template
-import sqlalchemy
-from sqlalchemy import create_engine
+import pypyodbc as pp
 
 
 app = Flask(__name__)
@@ -26,9 +25,18 @@ def home():
     return render_template('index.html')
 
 
-def ChAnGe_LaTeR(): #function for connecting to SQL server
-    engine = create_engine() # figure out later
+@app.route("/sql")
+def sql():
+    return sql_connect()
 
+
+def sql_connect(): #function for connecting to SQL server
+    try:
+        connection = pp.connect('Driver= {sqlsrv};Server = localhost\\SQLEXPRESS;Database = rms; '
+                                'uid=rmsapp;pwd=ss1RMSpw@wb02')# figure out later
+    except pp.Error:
+        return render_template('error.html')
+    return render_template('sql.html')
 
 if __name__ == "__main__":
     app.run()
