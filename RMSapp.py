5105b4f722f5a523076f595c9271ad0b75ad6e69
beffypuffs <56742527+beffypuffs@users.noreply.github.com>
2021-10-16 17:56:29 -0700

    Connected to SQL server, reverted index.html and chocks.html

--- RMSapp.py
+++ RMSapp.py
@@ -25,18 +25,41 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/sql")
-def sql():
-    return sql_connect()
+#Replace with /index when its finished
+@app.route("/queryresults")
+def query():
+    return query_results()
 
 
-def sql_connect(): #function for connecting to SQL server
-    try:
-        connection = pp.connect('Driver= {sqlsrv};Server = localhost\\SQLEXPRESS;Database = rms; '
-                                'uid=rmsapp;pwd=ss1RMSpw@wb02')# figure out later
-    except pp.Error:
-        return render_template('error.html')
-    return render_template('sql.html')
+
+def query_results(): #Displays roll information
+    try: #Use this code whenever you connect to SQL server
+        connection = pp.connect('Driver= {SQL Server};Server=localhost\\SQLEXPRESS;Database=rms;'
+        'uid=rmsapp;pwd=ss1RMSpw@wb02') 
+    except pp.Error as e:
+        message = "error connecting to SQL Server: " + str(e) #returns error type
+        return message
+    cur = connection.cursor() #used to execute actions, might be able do more idk
+    cur.execute("Select *  From roll ORDER BY roll_num DESC")#query
+    data = cur.fetchall()
+    page = "<!DOCTYPE html><html lang = \"en\"><body>"#May be a better way to have html files interact with flask, right now it just writes the html each time this runs
+    for row in data:
+        page += "=============<br>"
+        page += "roll_num: " + str(row[0]) + "<br>"
+        page += "roll_mate: " + str(row[1])+ "<br>" #always null, ask about it on monday
+        page += "current_diameter: " + str(row[2]) + "<br>"
+        page += "starting_diameter: " + str(row[3]) + "<br>"
+        page += "mill: " + str(row[4]) + "<br>"
+        page += "roll_type: " + str(row[5]) + "<br>"
+        page += "status: " + str(row[6]) + "<br>" #also null
+        page += "manufactured_date: " + str(row[7]) + "<br>"#ditto
+    page += "</body></html>"
+    return page
+
+
+    
+    #return render_template('sql.html')
+
 
 if __name__ == "__main__":
     app.run()
