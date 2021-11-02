32d6a8b34fdf6abe438ab76cf307e4a43fff7157
JSew11 <jseward@zagmail.gonzaga.edu>
2021-11-01 20:30:12 -0700

    Began work on input sanitation for email list

---
+++ InputSanitation.py
@@ -0,0 +1,9 @@
+"""
+Python file to provide input sanitation for the Kaiser RMS
+Written By - Joshua Seward
+"""
+import re
+
+# input sanitation for adding emails to the notifications list
+def email_input_sanitation():
+    return
---
+++ Test_InputSanitation.py
@@ -0,0 +1,6 @@
+"""
+Testing file for RMS Input Sanitation
+Written By - Joshua Seward
+"""
+
+import pytest
