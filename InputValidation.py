c1aa3c6958bfab6e9bdf16bb732fdb5baacd5b30
JSew11 <jseward@zagmail.gonzaga.edu>
2021-11-05 13:09:27 -0700

    Wrote vaildation for email usernames and domains

--- InputValidation.py
+++ InputValidation.py
@@ -6,5 +6,12 @@ import re # library to use regexes
 
 # input validation that checks for valid Kaiser email domains
 def domain_validation(domain):
-    domain_match = re.match("@kaisertwd.com", domain, re.I)
+    # check the given domain against valid kaiser email domains
+    domain_match = re.match("^@(kaisertwd.com|KaiserTWD.com)$", domain)
     return bool(domain_match)
+
+# input validation that checks for valid email usernames
+def username_validation(username):
+    # check the given username against the valid characters for an email username
+    username_match = re.match("^([a-z]|[A-Z]|[0-9])*$", username)
+    return bool(username_match)
--- tests/test_InputValidation.py
+++ tests/test_InputValidation.py
@@ -6,11 +6,57 @@ from re import T
 from typing import ValuesView
 import InputValidation as validation
 
-# check that a generic email from kaiser passes through sanitization
+# check that a generic kaiser domain passes (lowercase)
 def test_valid_email_domain_1():
     valid_email_domain = '@kaisertwd.com'
     assert validation.domain_validation(valid_email_domain) == True
 
+# check that a generic kaiser domain passes (PascalCase)
 def test_valid_email_domain_2():
     valid_email_domain = '@KaiserTWD.com'
     assert validation.domain_validation(valid_email_domain) == True
+    
+# check that a domain without the '@' character fails
+def test_invalid_email_domain_1():
+    invalid_email_domain = 'kaisertwd.com'
+    assert validation.domain_validation(invalid_email_domain) == False
+    
+# check that a domain ending with an invalid character fails
+def test_invalid_email_domain_2():
+    invalid_email_domain = '@kaisertwd.com\''
+    assert validation.domain_validation(invalid_email_domain) == False
+
+# check that a domain starting with an invalid character fails
+def test_invalid_email_domain_3():
+    invalid_email_domain = '\\@kaisertwd.com'
+    assert validation.domain_validation(invalid_email_domain) == False
+
+# check that a domain including an invalid character fails
+def test_invalid_email_domain_4():
+    invalid_email_domain = 'Kaiser$TWD.com'
+    assert validation.domain_validation(invalid_email_domain) == False
+
+# check that a valid kaiser username passes (lastname)
+def test_valid_email_username_1():
+    valid_email_username = 'lastname'
+    assert validation.username_validation(valid_email_username) == True
+
+# check that a valid outside vendor username passes (ov#####)
+def test_valid_email_username_2():
+    valid_email_username = 'ov12345'
+    assert validation.username_validation(valid_email_username) == True
+
+# check that a username beginning with an invalid character fails
+def test_invalid_email_username_1():
+    invalid_email_username = '\'lastname'
+    assert validation.username_validation(invalid_email_username) == False
+
+# check that a username ending with an invalid character fails
+def test_invalid_email_username_2():
+    invalid_email_username = 'lastname\\'
+    assert validation.username_validation(invalid_email_username) == False
+    
+# check that a username that includes an invalid character fails
+def test_invalid_email_username_3():
+    invalid_email_username = 'last$name'
+    assert validation.username_validation(invalid_email_username) == False
