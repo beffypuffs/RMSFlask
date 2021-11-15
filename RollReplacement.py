d81fecce0daec397e3010d9dfe5284551791b05a
JSew11 <jseward@zagmail.gonzaga.edu>
2021-11-15 11:12:07 -0800

    Tried to fix predictive model query

--- RollReplacement.py
+++ RollReplacement.py
@@ -17,7 +17,7 @@ def days_until_replacement(roll_id):
         try:
             cur.execute(f'SELECT r.current_diameter, ri.scrap_diameter, ri.avg_grind_diameter, ri.days_between_rolls'
                          'FROM roll r JOIN roll_info ri ON (r.mill = ri.mill AND r.roll_type = ri.roll_type)'
-                         'WHERE r.roll_num = \'%d\';', roll_id)
+                         'WHERE r.roll_num = {roll_id};')
             # get the matching first row of data and use it to predict the remaining roll life
             query_results = cur.fetchall()
             cur_diameter = float(query_results[0][0])
