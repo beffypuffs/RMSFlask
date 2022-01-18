88e360e2267033987e420b70df21e0c12bac9de2
beffypuffs <56742527+beffypuffs@users.noreply.github.com>
2022-01-18 15:31:27 -0800

    Add files via upload

--- Connections.py
+++ Connections.py
@@ -1,10 +1,16 @@
 # anything connecting to SQL or access will be here for now
 
 import datetime
+import numpy as np
 import pyodbc as pp
+import psycopg2
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 
-def access_connect():
+def access_connect(): #irrelevant now
     try:
         conn = pp.connect('Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=E:\\RMS\\Access\\RSMS_IF.NET.mdb;')
     except pp.Error as e:
@@ -24,7 +30,7 @@ def access_connect():
         # print(insert)
         cur.execute(insert)
     delete_dups(S_conn)
-    remove_fake_data(S_conn)
+    #remove_fake_data(S_conn)
     S_conn.commit()
 
 
@@ -57,12 +63,12 @@ def delete_dups(connection):#deletes duplicates using CTE. Can be simplified if 
     cur.execute('WITH CTE([Entry_Time], dups) AS (SELECT [Entry_Time], ROW_NUMBER() OVER (PARTITION BY [Entry_Time] ORDER BY Id) AS dups FROM Grind_Raw) DELETE FROM CTE WHERE dups > 1')
 
 
-def remove_fake_data(connection):#SQL data has some fake entries, won't need this function once we are getting live data
-    cur = connection.cursor()
-    cur.execute('DELETE FROM Grind_Raw WHERE Entry_Time > \'2020-09-16 8:00:00\'')
+# def remove_fake_data(connection):#SQL data has some fake entries, won't need this function once we are getting live data
+#     cur = connection.cursor()
+#     cur.execute('DELETE FROM Grind_Raw WHERE Entry_Time > \'2020-09-16 8:00:00\'')
 
 
-def remove_email(request):
+def remove_email(connection, data):
     committed = False
     message = ""
     try: #Use this code whenever you connect to SQL server
@@ -72,9 +78,9 @@ def remove_email(request):
         message = ('error connecting to SQL server: ' + str(e))
         return committed, message
 
-    badge_number = request.form['badge_number']
-    name = request.form['nm']
-    email = request.form['email']
+    badge_number = data[0]
+    name = data[1]
+    email = data[2]
 
     cur = connection.cursor()
     try:
@@ -87,20 +93,12 @@ def remove_email(request):
     return committed, message
 
 
-def add_email(request):
+def add_email(connection, data):
     committed = False
     message = ""
-    try: #Use this code whenever you connect to SQL server
-        connection = pp.connect('Driver= {SQL Server};Server=localhost\\SQLEXPRESS;Database=rms;'
-            'uid=rmsapp;pwd=ss1RMSpw@wb02') 
-    except pp.Error as e:
-        message = ("error connecting to SQL Server: " + str(e)) #returns error type
-        return committed, message
-
-    badge_number = request.form['badge_number']
-    name = request.form['nm']
-    email = request.form['email']
-
+    badge_number = data[0]
+    name = data[1]
+    email = data[2]
     cur = connection.cursor()
     try:
         cur.execute('INSERT INTO employee VALUES(' + badge_number + ', \'' + name + '\', \'' + email + '\')')
@@ -112,15 +110,9 @@ def add_email(request):
     return committed, message
         
 
-def query_results(query, cols): #Displays roll information
+def query_results(connection, query, cols): #Displays roll information
     executed = False
     message = ""
-    try: #Use this code whenever you connect to SQL server
-        connection = pp.connect('Driver= {SQL Server};Server=localhost\\SQLEXPRESS;Database=rms;'
-            'uid=rmsapp;pwd=ss1RMSpw@wb02') 
-    except pp.Error as e:
-        message = "error connecting to SQL Server: " + str(e) #returns error type
-        return None, executed, message
     cur = connection.cursor() # Used to execute actions, might be able do more idk
     try:
         cur.execute(query) # Query
@@ -139,87 +131,105 @@ def query_results(query, cols): #Displays roll information
     executed = True
     return table_data, executed, message
 
-def add_chock(request):
+def add_chock(connection, data):
     committed = False
     message = ""
+    cur = connection.cursor()
+    #INPUT SANITATION
+    #print('INSERT INTO employee VALUES(' + badge_number + ', \'' + name + '\', \'' + email + '\')')
+    try:
+        message = sql_insert('report', data)
+        cur.execute(message)
+       #  cur.execute(f'INSERT INTO report VALUES({date}, {chock_nu')
+    except pp.Error as e:
+        return committed, str(e) #returns error code if query fails
+    connection.commit()
+    committed = True
+    return committed, message
+
+def remove_chock(connection, data):
+    committed = False
+    message = ""
+    cur = connection.cursor()
+    date = data[0]
+    badge_num = data[52]
+
+    # comments = request.form['comments']
+    cur.execute(f'DELETE FROM report WHERE date = \'{date}\' AND badge_number = \'{badge_num}\'')
+    connection.commit()
+    return True, "Successfully removed"
+
+def edit_chock(connection, data): #only works when date and bage_number are not changed, needs work in the future
+    committed, message = remove_chock(connection, data)
+    if (committed is True):
+        committed, message = add_chock(connection, data)
+    else:
+        print (committed)
+        print(message)
+    return committed, message
+
+def sql_connect(): #not used, eventually will return current sql connection or start a new one if it hasn't been called
+    # connection = psycopg2.connect(dbname='RMS',user='rmsAdmin',password='1029384756', host '')  #change to your ip
+    message = "connected"
     try: #Use this code whenever you connect to SQL server
         connection = pp.connect('Driver= {SQL Server};Server=localhost\\SQLEXPRESS;Database=rms;'
     'uid=rmsapp;pwd=ss1RMSpw@wb02') 
     except pp.Error as e:
         message = "error connecting to SQL Server: " + str(e) #returns error type
-        return committed, message
+        print(message)
+        return None, message
+    return connection, message
 
-    date = request.form['date'],
-    chock_number = request.form['chock-num'],
-    position = request.form['position'],
-    reason = request.form['reasons_d_and_i'],
-    visible_chock_numbers = request.form['obvi'],
-    lifiting_bolt_thread_condition = request.form['lifting'],
-    cover_end_condition = request.form['cover'],
-    bell_o_ring_condition = request.form['end-bell'],
-    thrust_collar = request.form['thrust'],
-    lockkeepers = request.form['locks'],
-    liner_plates = request.form['liner'],
-    inboard_radial_seals_replaced = request.form['num-rep'],
-    inboard_face_seal = request.form['seals1'],
-    outboard_radial_seal = request.form['seals2'],
-    load_zone_from_mill = request.form['mill1'],
-    load_zone_to_mill = request.form['mill2'],
-    bearing_grease_condition = request.form['bearing-grease'],
-    bearing_mfg = request.form['mfg'],
-    bearing_serial_number = request.form['sn'],
-    is_sealed = request.form['sealed'],
-    seals_replaced = request.form['seals-rep'],
-    cup_a = request.form['cupA'],
-    cup_bd = request.form['cupB'],
-    cup_e = request.form['cupE'],
-    race_a = request.form['raceA'],
-    race_b = request.form['raceB'],
-    race_d = request.form['raceD'],
-    race_e = request.form['raceE'],
-    bearing_status = request.form['bearing-condition'],
-    different_bearing_installed = request.form['diff-bearing'],
-    bearing_mfg_new = request.form['textMFG'],
-    serial_number_new = request.form['MFGsn'],
-    sealed_new = request.form['sealed2'],
-    chock_bore_round = request.form['chockBoreRound'],
-    chock_bore = request.form['chockBoreOOR'],
-    no_rust = request.form['wearOrRust'],
-    grease_purged = request.form['purgeGrease'],
-    spots_dings = request.form['spots-dings'],
-    manual_pack = request.form['manual-pack'],
-    lube_bore = request.form['lube-bore'],
-    grease_packed_bearings = request.form['dropped'],
-    height_shoulder = request.form['droppedA'],
-    bearing_depth = request.form['droppedB'],
-    shims_needed = request.form['droppedDifference'],
-    was_paper_used = request.form['paper-used'],
-    by_hand = request.form['shim'],
-    was_torqued = request.form['phases'],
-    ancillary_installed = request.form['ancillary'],
-    grease_pack_sealed = request.form['greasePack'],
-    chock_ready_for_installation = request.form['ready'],
-    comments  = request.form['comments'],
-    mill = request.form['roll_mill'],
-    badge_number = request.form['badge_number'],
-    roll_type = request.form['roll_type']
-    cur = connection.cursor()
-    #INPUT SANITATION
-    #print('INSERT INTO employee VALUES(' + badge_number + ', \'' + name + '\', \'' + email + '\')')
-    try:
-        # first draft of this! I need to ask jeff what some values should be and check which ones are numbers since I'm assuming most of these are being interpreted as strings
-        cur.execute('INSERT INTO report VALUES(' + date + ', ' + chock_number + ', \'' + position + '\', \'' + reason + '\', \'' + visible_chock_numbers + '\', \'' + lifiting_bolt_thread_condition + 
-        '\', \'' + cover_end_condition + '\', \'' + bell_o_ring_condition + '\', \'' + thrust_collar + '\', \'' + '\', \'' + lockkeepers + '\', \'' + liner_plates + '\', \'' + inboard_radial_seals_replaced +
-        '\', \'' + inboard_face_seal + '\', \'' + outboard_radial_seal + '\', \'' + load_zone_from_mill + '\', \'' + load_zone_to_mill + '\', \'' + bearing_grease_condition + '\', \'' + bearing_mfg +
-        '\', \'' + bearing_serial_number + '\', \'' + is_sealed + '\', \'' + seals_replaced + '\', \'' + cup_a + '\', \'' + cup_bd + '\', \'' + cup_e + '\', \'' + race_a + '\', \'' + race_b +
-        '\', \'' + race_d + '\', \'' + race_e + '\', \'' + bearing_status + '\', \'' + different_bearing_installed + '\', \'' + bearing_mfg_new + '\', \'' + serial_number_new + '\', \'' + sealed_new +
-        '\', \'' + chock_bore_round + '\', \'' + chock_bore + '\', \'' + no_rust + '\', \'' + grease_purged + '\', \'' + spots_dings + '\', \'' + manual_pack + '\', \'' + lube_bore + '\', \'' + grease_packed_bearings + 
-        '\', \'' + height_shoulder + '\', \'' + bearing_depth + '\', \'' + shims_needed + '\', \'' + was_paper_used + '\', \'' + by_hand + '\', \'' + was_torqued + '\', \'' + ancillary_installed +
-        '\', \'' + grease_pack_sealed + '\', \'' + chock_ready_for_installation + '\', \'' + comments + '\', \'' + mill + '\', \'' + badge_number + '\', \'' + roll_type + 
-        '\')')
-    except pp.Error as e:
-        return str(e) #returns error code if query fails
-    connection.commit()
-    committed = True
-    return committed, message
+def generate_graphs(roll_num): #not useful rn just messing around with matplotlib, we need a dataset for multiple rolls that we can use until we have enough data
+    #connection, message = sql_connect()
+    #cur = connection.cursor()
+    # roll_num = 1619
+    # cur.execute(f'SELECT Roll_diameter_HS_After, Entry_Time FROM Grind_Raw WHERE Roll_Number={roll_num} ORDER BY Entry_Time')
+    # data = cur.fetchall()
+    y1 = [130.60,130.54, 130.48, 130.4, 130.32, 129.4, 129.32, 129.24, 129.16, 129.08, 129, 128.9, 128.8, 128.7, 128.6, 128.5, 128.4, 128.3, 128.2, 128.1, 128, 127.9, 127.8, 127.7, 127.6, 127.5]
+    x = [datetime.datetime(2021, 3, 10, 12, 12, 12), datetime.datetime(2021, 4, 12, 12, 12, 12), datetime.datetime(2021, 5, 14, 12, 12, 12),
+        datetime.datetime(2021, 6, 17, 12, 12, 12), datetime.datetime(2021, 7, 20, 12, 12, 12), datetime.datetime(2021,8, 23, 12, 12, 12),
+        datetime.datetime(2021, 9, 24, 12, 12, 12), datetime.datetime(2021, 10, 28, 12, 12, 12), datetime.datetime(2021, 12, 1, 12, 12, 12),
+        datetime.datetime(2022, 1, 1, 12, 12, 12), datetime.datetime(2022, 2, 1, 12, 12, 12), datetime.datetime(2022, 3, 1, 12, 12, 12), datetime.datetime(2022, 4, 1, 12, 12, 12),
+        datetime.datetime(2022, 5, 1, 12, 12, 12), datetime.datetime(2022, 6, 1, 12, 12, 12), datetime.datetime(2022, 7, 1, 12, 12, 12), datetime.datetime(2022, 8, 1, 12, 12, 12), 
+        datetime.datetime(2022, 9, 1, 12, 12, 12), datetime.datetime(2022, 10, 1, 12, 12, 12), datetime.datetime(2022, 11, 1, 12, 12, 12), datetime.datetime(2022, 12, 1, 12, 12, 12),
+        datetime.datetime(2023, 1, 1, 12, 12, 12), datetime.datetime(2023, 2, 1, 12, 12, 12), datetime.datetime(2023, 3, 1, 12, 12, 12), datetime.datetime(2023, 4, 1, 12, 12, 12),
+        datetime.datetime(2023, 5, 1, 12, 12, 12)]
+    # for row in data:
+    #     x.append(row[1])
+    #     y.append(row[0])
+    fig, ax = plt.subplots()
+    ax.plot_date(x, y1, markerfacecolor = 'CornflowerBlue', markeredgecolor = 'Red', zorder=10)
+    plt.axhline(y=120, color='y', linestyle='-')
+    plt.axhline(y=117, color='r', linestyle='-')
+    fig.autofmt_xdate()
+    ax.set_xlim([datetime.date(2020, 12, 25), datetime.date(2030, 2, 1)])
+    ax.set_ylim(100, 140)
+    x2 = np.array([datetime.date(2020, 12, 25), datetime.date(2030, 2, 1)])
+    y2 = np.array([130.60, 118])
+    plt.plot(x2, y2, color='g', zorder=0)
+    ax.title.set_text(f'Diameter Over Time: Roll {roll_num}')
+    plt.legend(['Grinds', 'Needs Replacement', 'Scrapped', 'Projection'])
+    plt.xlabel('Date')
+    plt.ylabel('Diameter (in.)')
+    plt.savefig('static\\images\\Sample Graph.png')
+    
+    
+    # fig.title('Diameter Over Time')
+    
+    # plt.scatter(x,y)
+
+    # # plt.xaxis.set_major_locator(mdates.MonthLocator(bymonthday=(1, 15)))
+    # plt.xlabel('Grind Date/Time')
+    # plt.ylabel('Diameter')
+    # plt.title('Diameter over time')
+    # plt.savefig('static\\images\\Sample Graph.png')
+    
+    # return plt
+
+# generate_graphs()
+
+
+
+    
     
--- RMSapp.py
+++ RMSapp.py
@@ -1,6 +1,7 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, g
 from flask_mail import Mail, Message
 import Connections
+import Requests
 
 
 app = Flask(__name__)
@@ -22,18 +23,32 @@ def help_page():
 
 @app.route("/chocksMenu")
 def chocksMenu():
-    return render_template('chocksMenu.html')
+    return render_template('chocks.html')
 
 
-@app.route("/chocksView")
+@app.route("/chocksView", methods = ['GET','POST']) 
 def chocksView():
-    headings = ("Roll ID", "Status", "Current Diameter", "Starting Diameter", "Mill", "Roll Type", "Manufacture Date")
-    data, committed, message = Connections.query_results("Select *  FROM report ORDER BY date DESC", 7)
-    if committed is True:
-        return render_template('chocksView.html', headings=headings, data=data)
+    if request.method == 'POST':
+        i = int(request.form['form_num'])
+        next_or_prev = request.form['submitResponse']
+        if next_or_prev == "Next form":
+            i -= 1
+        else:
+            i += 1
+        connection, message = Connections.sql_connect()
+        data, committed, message = Connections.query_results(connection, "Select *  FROM report ORDER BY date DESC", 54)
+        if committed is True:
+                return render_template('chocksview2.html', data = data, i = i, length = len(data)) #right now it sends every form which i'll fix later
+        else:
+            return message
     else:
-        return message #error message, needs an html page
-    
+        connection, message = Connections.sql_connect()
+        data, committed, message = Connections.query_results(connection, "Select *  FROM report ORDER BY date DESC", 54)
+        if committed is True:
+            return render_template('chocksView2.html', data = data, i = 0, length = len(data)) #see above
+        else:
+            return message #error message, needs an html page
+
 
 @app.route("/chocks")
 def chocks():
@@ -48,8 +63,9 @@ def notifications():
 
 @app.route("/")
 def home():
-    headings = ("Roll ID", "Status", "Current Diameter", "Starting Diameter", "Mill", "Roll Type", "Manufacture Date")
-    data, committed, message = Connections.query_results("Select *  FROM roll ORDER BY roll_num DESC", 7)
+    headings = ("Roll ID", "Diameter", "Scrap Diameter", "Approx. Scrap Date", "Grinds Left", "Mill", "Roll Type")
+    connection, message = Connections.sql_connect()
+    data, committed, message = Connections.query_results(connection, "Select *  FROM roll_new ORDER BY approx_scrap_date ASC", 7)
     if committed is True:
         return render_template("index.html", headings=headings, data=data)
     else:
@@ -64,20 +80,38 @@ def query():
     return render_template('rollData.html')#temporary link
 
 #Add chock function
-@app.route('/add-chock', methods = ['GET','POST'])
+@app.route('/add_chock', methods = ['GET','POST'])
 def add_chock():
+    connection, message = Connections.sql_connect()
     if request.method == 'POST':
-        committed, message = Connections.add_chock(request)
-        if (committed is True):
-            return 'succesfully added chock' #maybe option to view all chocks forms after submitting
+        if (request.form['submitResponse'] == 'Submit Form'):
+            data = Requests.chock_request_data(request)
+            committed, message = Connections.add_chock(connection, data)
+            if (committed is True):
+                return render_template('successfulAdd.html') #maybe option to view all chocks forms after submitting
+            else:
+                return message #error message
+        elif (request.form['submitResponse'] == 'Remove Form'):
+            data = Requests.chock_request_data(request)
+            committed, message = Connections.remove_chock(connection, data)
+            if (committed is True):
+                return render_template('successfulRemove.html')
+            else:
+                return message
         else:
-            return message #error message
-    return 'thing'
+            data = Requests.chock_request_data(request)
+            committed, message = Connections.edit_chock(connection, data)
+            if (committed is True):
+                return render_template('successfulEdit.html')
+            else:
+                return message
 
 @app.route('/add-email', methods = ['GET','POST'])#template for saving data from a webpage
 def add_email():
     if request.method == 'POST':
-        committed, message = Connections.add_email(request)
+        connection = Connections.sql_connect()
+        data = Requests.email_request_data(request)
+        committed, message = Connections.add_email(connection, data)
         if committed is True:
             return 'email succesfully added'
         else:
@@ -87,13 +121,21 @@ def add_email():
 @app.route('/remove-email', methods = ['POST'])
 def remove_email():
     if request.method == 'POST':
-        committed, message = Connections.remove_email(request)
+        connection = Connections.sql_connect()
+        data = Requests.email_request_data(request)
+        committed, message = Connections.remove_email(connection, data)
         if committed is True:
             return 'email succesfully removed'
         else:
             return message #error message
     return 'thing'
 
+@app.route('/roll-view', methods = ['POST'])
+def roll_view():
+    if request.method == 'POST':
+        roll_num = request.form['roll_clicked']
+        Connections.generate_graphs(roll_num)
+        return render_template('rollView.html', graph=Connections.generate_graphs, roll_num = roll_num)
 
 def send_notification_email(roll_id):
     mail = Mail(app)
@@ -106,6 +148,17 @@ def send_notification_email(roll_id):
     message.add_recipient('rmsnotirecipient@gmail.com')
     mail.send(message)
     return 'Notification Email Sent'
+
+# def send_status_report():
+#     mail = Mail(app)
+#     #send current diameter and projected lifespan of each roll
+
+
+# @app.before_request
+# def set_db():
+#     if not hasattr(g, 'sqllite_db'):
+#         g.sqlite_db = Connections.sql_connect()
+#     return g.sqlite_db
 
 
 if __name__ == "__main__":
