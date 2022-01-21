58e598513abbedaa1de585ec2f9e3136a69cc092
JSew11 <jseward@zagmail.gonzaga.edu>
2022-01-20 16:09:05 -0800

    included notifications file in RMSapp.py

--- RMSapp.py
+++ RMSapp.py
@@ -4,9 +4,10 @@ from flask_mail import Mail, Message
 import pyodbc as pp
 import RollReplacement as rollRep
 import EmailValidation as eVal
-
+import Notifications as notif
 
 app = Flask(__name__)
+rms_mail = Mail(app)
 
 RMS_EMAIL = 'RMSNotifications1@gmail.com' # change for Kaiser email
 # settings for sending email notifications - NOT FINAL VALUES
@@ -219,39 +220,52 @@ def query_results(): #Displays roll information
     return table_data
 
 # function to send a notification email to the registered users in the RMS
-def send_notification_email(roll_id):
-    mail = Mail(app)
-    message = Message('TEST MESSAGE', sender='RMSNotifications1@gmail.com')
+def send_notification_email():
+    # SAMPLE DATA TO TEST NOTI EMAIL RENDERING
+    rolls_at_EOL = [[]]
+    rolls_near_EOL = [[]]
 
-    # NEED TO REFORMAT THIS WITH HTML - include something about the recipient being on
-    # the notifications list and how to get off of it
-    # INFO TO INCLUDE IN THE EMAIL - 
-    message.body = f'This email should say something about a new roll needing to be ordered\
-    (and include the roll_num: {roll_id} that is being replaced)'
+    # get rolls at (or past) the end of their lifespan
 
-    # ONLY WORKS WHEN CONNECTED TO KAISER NETWORK
-    try: # connect to RMS database REPLACE WITH Connections.py
-        connection = pp.connect('Driver= {SQL Server};Server=localhost\\SQLEXPRESS;Database=rms;'
-    'uid=rmsapp;pwd=ss1RMSpw@wb02') 
-        cur = connection.cursor()
-    except pp.Error as e: # couldn't connect to 
-        error_message = "error connecting to SQL Server: " + str(e) #returns error type
-        return error_message
+    # get rolls near the end of their lifespan
 
-    try: # query database to get emails we need to notify
-        cur.execute('SELECT email FROM employee;')
-        emails = cur.fetchall()
-        for row in emails: # add emails to the message as recipients
-            message.add_recipient(row[0])
-    except pp.Error as e: # the SQL query fails
-        return 'error getting emails: ' + str(e)
+    # get notification recipients from the RMS database
+    recipients = []
+
+    # send the notification email
+    notif.send_noti_email(rolls_at_EOL, rolls_near_EOL, RMS_EMAIL, recipients, rms_mail)
+
+    # OLD WAY OF SENDING NOTIFICATION EMAILS
+    # mail = Mail(app)
+    # message = Message('TEST MESSAGE', sender='RMSNotifications1@gmail.com')
+
+    # # NEED TO REFORMAT THIS WITH HTML - include something about the recipient being on
+    # # the notifications list and how to get off of it
+    # # INFO TO INCLUDE IN THE EMAIL - 
+    # message.body = f'This email should say something about a new roll needing to be ordered\
+    # (and include the roll_num: {roll_id} that is being replaced)'
+
+    # # ONLY WORKS WHEN CONNECTED TO KAISER NETWORK
+    # try: # connect to RMS database REPLACE WITH Connections.py
+    #     connection = pp.connect('Driver= {SQL Server};Server=localhost\\SQLEXPRESS;Database=rms;'
+    # 'uid=rmsapp;pwd=ss1RMSpw@wb02') 
+    #     cur = connection.cursor()
+    # except pp.Error as e: # couldn't connect to 
+    #     error_message = "error connecting to SQL Server: " + str(e) #returns error type
+    #     return error_message
+
+    # try: # query database to get emails we need to notify
+    #     cur.execute('SELECT email FROM employee;')
+    #     emails = cur.fetchall()
+    #     for row in emails: # add emails to the message as recipients
+    #         message.add_recipient(row[0])
+    # except pp.Error as e: # the SQL query fails
+    #     return 'error getting emails: ' + str(e)
     
-    # USING TEST EMAIL FOR NOW - should query database and add all registered recipients
-    # TEST EMAIL RECIPIENT CREDENTIALS - rmsnotirecipient@gmail.com / Rm$noT1s
-    message.add_recipient('rmsnotirecipient@gmail.com')
-    mail.send(message)
-    return 'Notification Email Sent'
-
+    # # USING TEST EMAIL FOR NOW - should query database and add all registered recipients
+    # # TEST EMAIL RECIPIENT CREDENTIALS - rmsnotirecipient@gmail.com / Rm$noT1s
+    # message.add_recipient('rmsnotirecipient@gmail.com')
+    # mail.send(message)
 
 if __name__ == "__main__":
     app.run()
